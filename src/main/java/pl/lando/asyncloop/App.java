f3777d7fae3be3e9c969e548f10211e161ceeba2
Jan Swiecki <jan.swiecki@gmail.com>
2014-11-04 22:45:52 +0100

    first commit

---
+++ src/main/java/pl/lando/asyncloop/App.java
@@ -0,0 +1,9 @@
+package pl.lando.asyncloop;
+
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("test123");
+    }
+
+}
---
+++ src/main/java/pl/lando/asyncloop/AsyncLoop.java
@@ -0,0 +1,7 @@
+package pl.lando.asyncloop;
+
+/**
+ * Created by LND on 2014-11-04.
+ */
+public class AsyncLoop {
+}
